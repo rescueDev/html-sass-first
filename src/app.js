6b3e0839caf38a48d03421d6a469153b58cf7a06
sa-borgia3 <sa.borgia3@gmail.com>
2020-12-17 16:08:07 +0100

    added vuejs

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
 console.log("provaJS");
+
+import Vue from "vue";
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    navbar: [
+      {
+        name: "Home",
+      },
+
+      {
+        name: "About",
+      },
+
+      {
+        name: "Contact",
+      },
+    ],
+  },
+});
